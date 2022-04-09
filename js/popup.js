f50f990aacd0d56a101b72efc4c1089e64ff92c9
on1RB <dudkevich91@gmail.com>
2022-04-09 22:44:42 +0300

    выполнил 8.1

--- js/popup.js
+++ js/popup.js
@@ -9,16 +9,25 @@ const similarUsers = createUsers();
 
 const similarListFragment = document.createDocumentFragment();
 
+const setElementValue = (selector, attribute, value) => {
+  const element = userElement.querySelector(selector);
+  if (value) {
+    element[attribute] = value;
+  } else {
+    element.classList.add('hidden');
+  }
+};
+
+const housingType = {
+  flat: 'Квартира',
+  bungalow: 'Бунгало',
+  house: 'Дом',
+  palace: 'Дворец',
+  hotel: 'Отель',
+};
+
 const addElementsPopup = ({author, offer}) => {
   const userElement = similarUserTemplate.cloneNode(true);
-  const setElementValue = (selector, attribute, value) => {
-    const element = userElement.querySelector(selector);
-    if (value) {
-      element[attribute] = value;
-    } else {
-      element.classList.add('hidden');
-    }
-  };
   setElementValue('.popup__avatar', 'src', author.avatar);
   setElementValue('.popup__title', 'textContent', offer.title);
   setElementValue('.popup__text--address', 'textContent', offer.address);
@@ -27,13 +36,6 @@ const addElementsPopup = ({author, offer}) => {
   setElementValue('.popup__text--time', 'textContent', `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`);
   setElementValue('.popup__description', 'textContent', offer.description);
   setElementValue('.popup__photo', 'src', offer.photos);
-  const housingType = {
-    flat: 'Квартира',
-    bungalow: 'Бунгало',
-    house: 'Дом',
-    palace: 'Дворец',
-    hotel: 'Отель',
-  };
   setElementValue('.popup__type', 'textContent', housingType[offer.type]);
   const featuresContainer = userElement.querySelector('.popup__features');
   const featuresList = featuresContainer.querySelectorAll('.popup__feature');
@@ -48,7 +50,5 @@ const addElementsPopup = ({author, offer}) => {
   });
   similarListFragment.append(userElement);
 };
-
-
 
 similarListElement.append(similarListFragment);
